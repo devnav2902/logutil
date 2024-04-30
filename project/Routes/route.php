0fd7a21f98d267cbcf214d7ca74b0d8cb5358b18
nguyynavu <nguyenanhvu.work@gmail.com>
2024-04-30 07:18:33 +0700

    update namespace for route

--- project/Routes/route.php
+++ project/Routes/route.php
@@ -6,6 +6,6 @@ Route::get('/log-util', function (){
     return 'hello';
 });
 
-Route::namespace('Devnav2902\Http\Controllers')->group(function () {
+Route::namespace('Devnav2902\LogUtil\Http\Controllers')->group(function () {
     Route::get('/', 'LogUtilController@index');
 });
